e25c27a957989157c1645d494ff3b8a2a36e86dc
SsAbrut <michael.gunawan2002@gmail.com>
2020-12-23 14:26:54 +0700

    Added change panel

--- student-wallet-uc/src/main/change_balance.java
+++ student-wallet-uc/src/main/change_balance.java
@@ -28,21 +28,160 @@ public class change_balance extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
-        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        jPanel2 = new javax.swing.JPanel();
+        jLabel4 = new javax.swing.JLabel();
+        student_number = new javax.swing.JTextField();
+        jLabel5 = new javax.swing.JLabel();
+        new_balance = new javax.swing.JTextField();
+        jLabel6 = new javax.swing.JLabel();
+        submit_balance = new javax.swing.JToggleButton();
+        jPanel1 = new javax.swing.JPanel();
+        back_balance = new javax.swing.JButton();
 
-        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
-        getContentPane().setLayout(layout);
-        layout.setHorizontalGroup(
-            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 400, Short.MAX_VALUE)
+        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        setMinimumSize(new java.awt.Dimension(787, 60));
+        getContentPane().setLayout(null);
+
+        jPanel2.setBackground(new java.awt.Color(251, 211, 176));
+        jPanel2.setMinimumSize(new java.awt.Dimension(787, 320));
+
+        jLabel4.setFont(new java.awt.Font("Tahoma", 1, 14)); // NOI18N
+        jLabel4.setText("New balance");
+
+        student_number.setFont(new java.awt.Font("Tahoma", 0, 14)); // NOI18N
+
+        jLabel5.setFont(new java.awt.Font("Tahoma", 1, 14)); // NOI18N
+        jLabel5.setText("Student number");
+
+        new_balance.setFont(new java.awt.Font("Tahoma", 0, 14)); // NOI18N
+        new_balance.setMinimumSize(new java.awt.Dimension(787, 60));
+
+        jLabel6.setFont(new java.awt.Font("Tahoma", 0, 18)); // NOI18N
+        jLabel6.setText("Rp. ");
+
+        submit_balance.setBackground(new java.awt.Color(64, 191, 64));
+        submit_balance.setFont(new java.awt.Font("Tahoma", 1, 14)); // NOI18N
+        submit_balance.setText("Change balance");
+        submit_balance.setBorder(null);
+        submit_balance.setCursor(new java.awt.Cursor(java.awt.Cursor.HAND_CURSOR));
+        submit_balance.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                submit_balanceActionPerformed(evt);
+            }
+        });
+
+        javax.swing.GroupLayout jPanel2Layout = new javax.swing.GroupLayout(jPanel2);
+        jPanel2.setLayout(jPanel2Layout);
+        jPanel2Layout.setHorizontalGroup(
+            jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(jPanel2Layout.createSequentialGroup()
+                .addGap(30, 30, 30)
+                .addGroup(jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                    .addGroup(jPanel2Layout.createSequentialGroup()
+                        .addComponent(jLabel6)
+                        .addGap(15, 15, 15)
+                        .addComponent(new_balance, javax.swing.GroupLayout.PREFERRED_SIZE, 154, javax.swing.GroupLayout.PREFERRED_SIZE))
+                    .addComponent(jLabel5)
+                    .addComponent(jLabel4)
+                    .addComponent(student_number)
+                    .addComponent(submit_balance, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
         );
-        layout.setVerticalGroup(
-            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 300, Short.MAX_VALUE)
+        jPanel2Layout.setVerticalGroup(
+            jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(jPanel2Layout.createSequentialGroup()
+                .addGap(39, 39, 39)
+                .addComponent(jLabel5)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                .addComponent(student_number, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(30, 30, 30)
+                .addComponent(jLabel4)
+                .addGap(20, 20, 20)
+                .addGroup(jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(new_balance, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(jLabel6))
+                .addGap(30, 30, 30)
+                .addComponent(submit_balance, javax.swing.GroupLayout.PREFERRED_SIZE, 46, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap(93, Short.MAX_VALUE))
         );
+
+        getContentPane().add(jPanel2);
+        jPanel2.setBounds(0, 60, 770, 350);
+
+        jPanel1.setBackground(new java.awt.Color(255, 255, 255));
+        jPanel1.setMinimumSize(new java.awt.Dimension(787, 60));
+
+        back_balance.setBackground(new java.awt.Color(255, 255, 255));
+        back_balance.setFont(new java.awt.Font("Tahoma", 1, 14)); // NOI18N
+        back_balance.setText("<- Back");
+        back_balance.setBorder(javax.swing.BorderFactory.createLineBorder(new java.awt.Color(255, 255, 255), 2));
+        back_balance.setContentAreaFilled(false);
+        back_balance.setCursor(new java.awt.Cursor(java.awt.Cursor.HAND_CURSOR));
+        back_balance.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                back_balanceActionPerformed(evt);
+            }
+        });
+
+        javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
+        jPanel1.setLayout(jPanel1Layout);
+        jPanel1Layout.setHorizontalGroup(
+            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(jPanel1Layout.createSequentialGroup()
+                .addGap(23, 23, 23)
+                .addComponent(back_balance)
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+        );
+        jPanel1Layout.setVerticalGroup(
+            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(jPanel1Layout.createSequentialGroup()
+                .addGap(15, 15, 15)
+                .addComponent(back_balance, javax.swing.GroupLayout.PREFERRED_SIZE, 26, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap(19, Short.MAX_VALUE))
+        );
+
+        getContentPane().add(jPanel1);
+        jPanel1.setBounds(0, 0, 770, 60);
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
+
+    private void submit_balanceActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_submit_balanceActionPerformed
+        int amount = Integer.valueOf(insert_withdrawal.getText());
+        if (amount < 10000){
+            JOptionPane.showMessageDialog(this, "Minimum withdraw amount is Rp.10.000");
+        } else if(amount >= 10000){
+            if(amount < balance - 100000){
+                try{
+                    balance = balance - amount;
+                    sql = "UPDATE user SET balance=" + balance + " WHERE user_id=" + user_id + ";";
+                    stmt.execute(sql);
+                    insert_withdrawal.setText("");
+                    DecimalFormat rp = (DecimalFormat) DecimalFormat.getCurrencyInstance();
+                    DecimalFormatSymbols formatRp = new DecimalFormatSymbols();
+
+                    formatRp.setCurrencySymbol("Rp. ");
+                    formatRp.setMonetaryDecimalSeparator(',');
+                    formatRp.setGroupingSeparator('.');
+                    rp.setDecimalFormatSymbols(formatRp);
+                    JOptionPane.showMessageDialog(this, "Your total balance is now " + rp.format(balance) + ".");
+                    withdrawal.setText(rp.format(balance));
+                    java.util.Date date=java.util.Calendar.getInstance().getTime();
+                    sql = "INSERT INTO history (user_id, type, amount, date) VALUE("+ user_id +", 'Withdraw', " + amount + ", '" + date + "');";
+                    stmt.execute(sql);
+                } catch(Exception e){
+                    JOptionPane.showMessageDialog(this, e.getMessage());
+                }
+            } else {
+                JOptionPane.showMessageDialog(this, "Minimum balance left after withdrawal is Rp. 100.000");
+            }
+        }
+    }//GEN-LAST:event_submit_balanceActionPerformed
+
+    private void back_balanceActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_back_balanceActionPerformed
+        new MainMenu(user_id).setVisible(true);
+        dispose();
+    }//GEN-LAST:event_back_balanceActionPerformed
 
     /**
      * @param args the command line arguments
@@ -80,5 +219,14 @@ public class change_balance extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton back_balance;
+    private javax.swing.JLabel jLabel4;
+    private javax.swing.JLabel jLabel5;
+    private javax.swing.JLabel jLabel6;
+    private javax.swing.JPanel jPanel1;
+    private javax.swing.JPanel jPanel2;
+    private javax.swing.JTextField new_balance;
+    private javax.swing.JTextField student_number;
+    private javax.swing.JToggleButton submit_balance;
     // End of variables declaration//GEN-END:variables
 }
